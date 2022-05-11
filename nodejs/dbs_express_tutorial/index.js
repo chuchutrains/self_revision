b1ec34b7ceedff3e7580d36476dd7bfd293006b1
chuchutrains <chu_han93@hotmail.com>
2022-05-11 18:14:06 +0800

    express: - Remove module folder (following the best practice). - Add gitignore.

--- nodejs/dbs_express_tutorial/index.js
+++ nodejs/dbs_express_tutorial/index.js
@@ -1,8 +1,3 @@
-// Youtube tutorial: https://www.youtube.com/watch?v=l8WPWK9mS5M.
-// 1. To run, open up the terminal and run "npm start".
-// 2. Open up a browser and go to http://localhost:5000.
-// 3. Use Postman app to do CRUD operation.
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js'
